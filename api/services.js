faa3bfb7496cd1cb8c3fcde45fcffd463507e622
itzzytopup <tharukaashinsa00@gmail.com>
2026-08-29 11:14:12 +0530

    Update services.js

--- api/services.js
+++ api/services.js
@@ -48,9 +48,4 @@ export default async function handler(req, res) {
     return res.status(response.status).json(data);
 
   } catch (error) {
-    return res.status(500).json({
-      success: false,
-      error: "Server error"
-    });
-  }
-}
+    return res.st
